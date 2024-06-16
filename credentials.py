06c08586dfd920bd5830afc3e3f6ddac066ffa45
mcejalvo <marc.cejalvo@gmail.com>
2024-06-16 17:32:48 +0200

    Streamlit secrets refactor

--- credentials.py
+++ credentials.py
@@ -1,17 +1,18 @@
-import os
 import gspread
-from gspread_dataframe import get_as_dataframe, set_with_dataframe
+from gspread_dataframe import get_as_dataframe
 import streamlit as st
+from oauth2client.service_account import ServiceAccountCredentials
 
+# Use credentials from Streamlit secrets
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["GOOGLE_APPLICATION_CREDENTIALS"])
+gc = gspread.authorize(credentials)
 
-# GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', "credentials.json")
-GOOGLE_APPLICATION_CREDENTIALS = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 DOC_ID = "1kUY8Suw4jS2MhtNRQcHhogf04WdV7ubMpHwnaCPlir0"
-gc = gspread.service_account(filename=GOOGLE_APPLICATION_CREDENTIALS)
+doc = gc.open_by_key(DOC_ID)
 
 def get_dataframe_from_sheet(sheet_name, skiprows=None):
-    df = get_as_dataframe(doc.worksheet(sheet_name), evaluate_formulas=True, skiprows=skiprows).dropna(how='all').dropna(how='all', axis='columns')
+    sheet = doc.worksheet(sheet_name)
+    df = get_as_dataframe(sheet, evaluate_formulas=True, skiprows=skiprows).dropna(how='all').dropna(how='all', axis='columns')
     return df
 
-doc = gc.open_by_key(DOC_ID)
 df = get_dataframe_from_sheet("Data")
