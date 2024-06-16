6b6eaff4ef0723bb5a74688d0d08bed0cae23ce4
mcejalvo <marc.cejalvo@gmail.com>
2024-06-16 17:28:14 +0200

    Using Streamlit credentials

--- credentials.py
+++ credentials.py
@@ -2,17 +2,10 @@ import os
 import gspread
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
-GOOGLE_APP_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+# GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', "credentials.json")
+GOOGLE_APPLICATION_CREDENTIALS = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 DOC_ID = "1kUY8Suw4jS2MhtNRQcHhogf04WdV7ubMpHwnaCPlir0"
-gc = gspread.service_account(filename=GOOGLE_APP_CREDENTIALS)
-
-creds_json = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-if creds_json:
-    creds_dict = json.loads(creds_json)
-    creds = Credentials.from_service_account_info(creds_dict)
-    gc = gspread.authorize(creds)
-else:
-    raise ValueError("No GOOGLE_APPLICATION_CREDENTIALS found in environment variables")
+gc = gspread.service_account(filename=GOOGLE_APPLICATION_CREDENTIALS)
 
 def get_dataframe_from_sheet(sheet_name, skiprows=None):
     df = get_as_dataframe(doc.worksheet(sheet_name), evaluate_formulas=True, skiprows=skiprows).dropna(how='all').dropna(how='all', axis='columns')
