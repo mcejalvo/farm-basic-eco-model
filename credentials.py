b2cc40691cce6bca3305162cdd2b38bb53b25cc8
mcejalvo <marc.cejalvo@gmail.com>
2024-06-16 17:11:19 +0200

    Fixing credentials

--- credentials.py
+++ credentials.py
@@ -6,9 +6,13 @@ GOOGLE_APP_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 DOC_ID = "1kUY8Suw4jS2MhtNRQcHhogf04WdV7ubMpHwnaCPlir0"
 gc = gspread.service_account(filename=GOOGLE_APP_CREDENTIALS)
 
-def load_spreadsheet():
-    doc = gc.open_by_key(DOC_ID)
-    return doc
+creds_json = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+if creds_json:
+    creds_dict = json.loads(creds_json)
+    creds = Credentials.from_service_account_info(creds_dict)
+    gc = gspread.authorize(creds)
+else:
+    raise ValueError("No GOOGLE_APPLICATION_CREDENTIALS found in environment variables")
 
 def get_dataframe_from_sheet(sheet_name, skiprows=None):
     df = get_as_dataframe(doc.worksheet(sheet_name), evaluate_formulas=True, skiprows=skiprows).dropna(how='all').dropna(how='all', axis='columns')
