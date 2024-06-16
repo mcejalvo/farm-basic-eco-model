747a23f276dd44f3ba39816047efff091f260f8d
mcejalvo <marc.cejalvo@gmail.com>
2024-06-16 16:55:46 +0200

    First credentials push

---
+++ credentials.py
@@ -0,0 +1,17 @@
+import os
+import gspread
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+
+GOOGLE_APP_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', "credentials.json")
+gc = gspread.service_account(filename=GOOGLE_APP_CREDENTIALS)
+
+def load_spreadsheet():
+    doc = gc.open_by_key(DOC_ID)
+    return doc
+
+def get_dataframe_from_sheet(sheet_name, skiprows=None):
+    df = get_as_dataframe(doc.worksheet(sheet_name), evaluate_formulas=True, skiprows=skiprows).dropna(how='all').dropna(how='all', axis='columns')
+    return df
+
+doc = gc.open_by_key("1kUY8Suw4jS2MhtNRQcHhogf04WdV7ubMpHwnaCPlir0")
+df = get_dataframe_from_sheet("Data")
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,12 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from credentials import *
+
+
 
 """
-# Welcome to Streamlit! Test! 
+# Welcome to Streamlit! ! 
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
@@ -13,28 +16,4 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.dataframe(df.style.highlight_max(axis=0))
